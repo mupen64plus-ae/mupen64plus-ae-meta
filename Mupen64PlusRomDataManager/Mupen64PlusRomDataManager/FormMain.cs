53c8cc1549cc734bc835e20d0a2d7899e5bd8304
littleguy77 <mattlichter@live.com>
2014-12-15 20:24:51 -0500

    wiki: Fix links in home page, cover art in kiosk versions.

--- Mupen64PlusRomDataManager/Mupen64PlusRomDataManager/FormMain.cs
+++ Mupen64PlusRomDataManager/Mupen64PlusRomDataManager/FormMain.cs
@@ -23,31 +23,31 @@ namespace Mupen64PlusRomDataManager
 
         private void buttonWiki_Click(object sender, EventArgs e)
         {
-            Dictionary<string, RomEntry> roms = LoadRomData("mupen64plus.ini");
+            Dictionary<string, RomDetail> roms = LoadRomData("mupen64plus.ini");
             Dictionary<string, GamePage> pages = AggregateGamePages(roms);
             GenerateWiki(pages);
         }
 
         private void buttonValidate_Click(object sender, EventArgs e)
         {
-            Dictionary<string, RomEntry> roms = LoadRomData("mupen64plus.ini");
+            Dictionary<string, RomDetail> roms = LoadRomData("mupen64plus.ini");
             Dictionary<string, GamePage> pages = AggregateGamePages(roms);
             ValidateDatabase(roms, pages);
         }
 
-        private static Dictionary<string, RomEntry> LoadRomData(string filename)
+        private static Dictionary<string, RomDetail> LoadRomData(string filename)
         {
             Regex md5 = new Regex(@"^\[([A-Fa-f0-9]+)\]$");
-            Regex goodname = new Regex("GoodName=(.+)");
             Regex crc = new Regex("CRC=(.+)");
+            Regex goodname = new Regex("GoodName=(.+)");
             Regex refmd5 = new Regex("RefMD5=(.+)");
+            Regex savetype = new Regex("SaveType=(.+)");
             Regex status = new Regex("Status=(.+)");
             Regex players = new Regex("Players=(.+)");
-            Regex savetype = new Regex("SaveType=(.+)");
             Regex rumble = new Regex("Rumble=(.+)");
 
-            Dictionary<string, RomEntry> roms = new Dictionary<string, RomEntry>();
-            RomEntry rom = new RomEntry(SECTIONLESS);
+            Dictionary<string, RomDetail> roms = new Dictionary<string, RomDetail>();
+            RomDetail rom = new RomDetail(SECTIONLESS);
             StreamReader reader = new StreamReader(filename);
             string line;
             while ((line = reader.ReadLine()) != null)
@@ -56,17 +56,20 @@ namespace Mupen64PlusRomDataManager
                 {
                     string value = md5.Match(line).Groups[1].Value;
                     roms.Add(rom.md5, rom);
-                    rom = new RomEntry(value);
+                    rom = new RomDetail(value);
                 }
-
-                if (goodname.IsMatch(line))
-                    rom.goodname = goodname.Match(line).Groups[1].Value;
 
                 if (crc.IsMatch(line))
                     rom.crc = crc.Match(line).Groups[1].Value;
 
+                if (goodname.IsMatch(line))
+                    rom.goodName = goodname.Match(line).Groups[1].Value;
+
                 if (refmd5.IsMatch(line))
                     rom.refmd5 = refmd5.Match(line).Groups[1].Value;
+
+                if (savetype.IsMatch(line))
+                    rom.saveType = savetype.Match(line).Groups[1].Value;
 
                 if (status.IsMatch(line))
                     rom.status = status.Match(line).Groups[1].Value;
@@ -74,46 +77,37 @@ namespace Mupen64PlusRomDataManager
                 if (players.IsMatch(line))
                     rom.players = players.Match(line).Groups[1].Value;
 
-                if (savetype.IsMatch(line))
-                    rom.savetype = savetype.Match(line).Groups[1].Value;
-
                 if (rumble.IsMatch(line))
                     rom.rumble = rumble.Match(line).Groups[1].Value;
             }
             roms.Remove(SECTIONLESS);
             reader.Close();
 
-            foreach (RomEntry entry in roms.Values)
+            foreach (RomDetail entry in roms.Values)
             {
-                RomEntry master;
+                RomDetail master;
                 if (entry.refmd5 != null && (master = roms[entry.refmd5]) != null)
                 {
                     entry.status = master.status;
                     entry.players = master.players;
-                    entry.savetype = master.savetype;
+                    entry.saveType = master.saveType;
                     entry.rumble = master.rumble;
                 }
             }
             return roms;
         }
 
-        private static Dictionary<string, GamePage> AggregateGamePages(Dictionary<string, RomEntry> roms)
+        private static Dictionary<string, GamePage> AggregateGamePages(Dictionary<string, RomDetail> roms)
         {
             Dictionary<string, GamePage> pages = new Dictionary<string, GamePage>();
-            foreach (RomEntry rom in roms.Values)
+            foreach (var rom in roms.Values)
             {
-                string title = rom.BaseName;
-                GamePage page;
-                if (pages.ContainsKey(title))
+                var title = rom.WikiTitle;
+                if (!pages.ContainsKey(title))
                 {
-                    page = pages[title];
+                    pages.Add(title, new GamePage(title));
                 }
-                else
-                {
-                    page = new GamePage(title);
-                    pages.Add(title, page);
-                }
-                page.roms.Add(rom);
+                pages[title].roms.Add(rom);
             }
             return pages;
         }
@@ -148,42 +142,27 @@ namespace Mupen64PlusRomDataManager
 
             StringBuilder builder = new StringBuilder();
             builder.AppendLine("## Games\n");
-            //for (int i = 0; i < titles.Count; i++)
-            //{
-            //    GamePage page = games[titles[i]];
-            //    string status = "Unknown";
-            //    string players = "Unknown";
-            //    string rumble = "Unknown";
-            //    string savetype = "Unknown";
-            //    if (page.roms.Count > 0)
-            //    {
-            //        RomEntry firstRom = page.roms[0];
-            //        status = firstRom.status;
-            //        players = firstRom.players;
-            //        rumble = firstRom.rumble;
-            //        savetype = firstRom.savetype;
-            //    }
-            //    builder.AppendFormat("- [[{0}]]\n", page.title);
-            //}
             builder.AppendLine("Name | Status | Players | Rumble | SaveType");
             builder.AppendLine("-----|--------|---------|--------|---------");
             for (int i = 0; i < titles.Count; i++)
             {
-                GamePage page = games[titles[i]];
+                var page = games[titles[i]];
+                string wikiLink = "";
                 string status = "Unknown";
                 string players = "Unknown";
                 string rumble = "Unknown";
                 string savetype = "Unknown";
                 if (page.roms.Count > 0)
                 {
-                    RomEntry firstRom = page.roms[0];
+                    RomDetail firstRom = page.roms[0];
+                    wikiLink = firstRom.WikiLink;
                     status = firstRom.status;
                     players = firstRom.players;
                     rumble = firstRom.rumble;
-                    savetype = firstRom.savetype;
+                    savetype = firstRom.saveType;
                 }
-                //builder.AppendFormat("[[{0}]] | {1} | {2} | {3} | {4}\n", page.title, status, players, rumble, savetype);
-                builder.AppendFormat("[{0}]({0}) | {1} | {2} | {3} | {4}\n", page.title, status, players, rumble, savetype);
+                //builder.AppendFormat("[[{0}]] | {1} | {2} | {3} | {4}\n", page.title, status, players, rumble, saveType);
+                builder.AppendFormat("[{0}]({1}) | {2} | {3} | {4} | {5}\n", page.title, wikiLink, status, players, rumble, savetype);
             }
  
             using (StreamWriter writer = new StreamWriter(OUT_FOLDER + "\\Home.md"))
@@ -192,25 +171,25 @@ namespace Mupen64PlusRomDataManager
             }
         }
 
-        private void ValidateDatabase(Dictionary<string, RomEntry> roms, Dictionary<string, GamePage> pages)
+        private void ValidateDatabase(Dictionary<string, RomDetail> roms, Dictionary<string, GamePage> pages)
         {
             richTextBox.Clear();
-            foreach (RomEntry rom in roms.Values)
+            foreach (RomDetail rom in roms.Values)
             {
                 if (rom.md5 == rom.refmd5)
-                    richTextBox.AppendText(rom.goodname + "  self reference\n");
+                    richTextBox.AppendText(rom.goodName + "  self reference\n");
 
                 if (rom.refmd5 != null)
                 {
                     if (!roms.ContainsKey(rom.refmd5))
-                        richTextBox.AppendText(rom.goodname + "  invalid reference\n");
+                        richTextBox.AppendText(rom.goodName + "  invalid reference\n");
                     else
                     {
                         var parent = roms[rom.refmd5];
                         if (parent.refmd5 != null)
-                            richTextBox.AppendText(rom.goodname + "  double-linked reference\n");
+                            richTextBox.AppendText(rom.goodName + "  double-linked reference\n");
                         if (parent.BaseName != rom.BaseName)
-                            richTextBox.AppendText(rom.goodname + "  incorrect reference\n");
+                            richTextBox.AppendText(rom.goodName + "  incorrect reference\n");
                     }
                 }
                 Application.DoEvents();
@@ -222,22 +201,22 @@ namespace Mupen64PlusRomDataManager
                 String players = null;
                 String savetype = null;
                 String rumble = null;
-                foreach (RomEntry rom in page.roms)
+                foreach (RomDetail rom in page.roms)
                 {
                     if (status != null && status != rom.status)
-                        richTextBox.AppendText(rom.goodname + "  status mismatch" + "\n");
+                        richTextBox.AppendText(rom.goodName + "  status mismatch" + "\n");
                     status = rom.status;
 
                     if (players != null && players != rom.players)
-                        richTextBox.AppendText(rom.goodname + "  players mismatch" + "\n");
+                        richTextBox.AppendText(rom.goodName + "  players mismatch" + "\n");
                     players = rom.players;
 
-                    if (savetype != null && savetype != rom.savetype)
-                        richTextBox.AppendText(rom.goodname + "  savetype mismatch" + "\n");
-                    savetype = rom.savetype;
+                    if (savetype != null && savetype != rom.saveType)
+                        richTextBox.AppendText(rom.goodName + "  saveType mismatch" + "\n");
+                    savetype = rom.saveType;
 
                     if (rumble != null && rumble != rom.rumble)
-                        richTextBox.AppendText(rom.goodname + "  rumble mismatch" + "\n");
+                        richTextBox.AppendText(rom.goodName + "  rumble mismatch" + "\n");
                     rumble = rom.rumble;
 
                     Application.DoEvents();
@@ -246,92 +225,18 @@ namespace Mupen64PlusRomDataManager
             richTextBox.AppendText("\nFinished checking database.");
         }
 
-        public class RomEntry
-        {
-            public readonly string md5;
-            public string goodname = null;
-            public string crc = null;
-            public string refmd5 = null;
-            public string status = null;
-            public string players = null;
-            public string savetype = null;
-            public string rumble = null;
-
-            public RomEntry(string md5)
-            {
-                this.md5 = md5;
-            }
-
-            public string SafeName
-            {
-                get
-                {
-                    return goodname.Replace('[', '(').Replace(']', ')');
-                }
-            }
-
-            public string BaseName
-            {
-                get
-                {
-                    String basename = SafeName.Split('(')[0].Trim();
-                    if (goodname.Contains("(Kiosk"))
-                        basename += " (Kiosk Demo)";
-                    return basename;
-                }
-            }
-
-            public string Description
-            {
-                get
-                {
-                    StringBuilder builder = new StringBuilder();
-
-                    if (refmd5 == null)
-                    {
-                        builder.AppendLine(";" + goodname);
-                        builder.AppendLine(": MD5: " + md5);
-                        builder.AppendLine(": CRC: " + crc);
-                        builder.AppendLine(": Status: " + status);
-                        builder.AppendLine(": Players: " + players);
-                        builder.AppendLine(": Save Type: " + savetype);
-                        builder.AppendLine(": Rumble: " + rumble);
-                    }
-                    else
-                    {
-                        builder.AppendLine(";" + goodname);
-                        builder.AppendLine(": MD5: " + md5);
-                        builder.AppendLine(": CRC: " + crc);
-                        builder.AppendLine(": See: " + refmd5);
-                    }
-
-                    return builder.ToString();
-                }
-            }
-        }
 
         public class GamePage
         {
             public readonly string title;
-            public readonly List<RomEntry> roms;
+            public readonly List<RomDetail> roms;
 
-            private const string artformat = @"![](http://paulscode.com/downloads/Mupen64Plus-AE/CoverArt/{0}.png)";
+            private const string ART_FORMAT = @"![]({0})";
 
             public GamePage(string title)
             {
                 this.title = title;
-                roms = new List<RomEntry>();
-            }
-
-            public string ArtName
-            {
-                get
-                {
-                    string name = title;
-                    name = Regex.Replace(name, @"['\.]", "");
-                    name = Regex.Replace(name, @"\W+", "_");
-                    return name;
-                }
+                roms = new List<RomDetail>();
             }
 
             public string BodyText
@@ -339,7 +244,7 @@ namespace Mupen64PlusRomDataManager
                 get
                 {
                     StringBuilder builder = new StringBuilder();
-                    builder.AppendLine(string.Format(artformat, ArtName));
+                    builder.AppendLine(string.Format(ART_FORMAT, roms.First().ArtUrl));
                     builder.AppendLine();
                     builder.AppendLine("## Recommended Settings");
                     builder.AppendLine();
@@ -347,9 +252,9 @@ namespace Mupen64PlusRomDataManager
                     builder.AppendLine();
                     builder.AppendLine("## Known Versions");
                     builder.AppendLine();
-                    foreach (var rom in roms.OrderBy(r => r.goodname))
+                    foreach (var rom in roms.OrderBy(r => r.goodName))
                     {
-                        builder.AppendLine("- `" + rom.goodname + "  " + rom.md5 + "`");
+                        builder.AppendLine("- `" + rom.goodName + "  " + rom.md5 + "`");
                     }
                     return builder.ToString();
                 }
---
+++ Mupen64PlusRomDataManager/Mupen64PlusRomDataManager/RomDetail.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Mupen64PlusRomDataManager
+{
+    public class RomDetail
+    {
+        private const String ART_URL_TEMPLATE = "http://paulscode.com/downloads/Mupen64Plus-AE/CoverArt/{0}";
+
+        public readonly string md5;
+        public string crc = null;
+        public string goodName = null;
+        public string refmd5 = null;
+        public string saveType = null;
+        public string status = null;
+        public string players = null;
+        public string rumble = null;
+
+        public RomDetail(string md5)
+        {
+            this.md5 = md5;
+        }
+
+        public string BaseName
+        {
+            get
+            {
+                var baseName = goodName;
+                baseName = Regex.Split(baseName, @" \(")[0];
+                baseName = Regex.Split(baseName, @" \[")[0];
+                return baseName.Trim();
+            }
+        }
+
+        public string ArtName
+        {
+            get
+            {
+                var artName = BaseName;
+                artName = Regex.Replace(artName, @"['\.]", "");
+                artName = Regex.Replace(artName, @"\W+", "_");
+                return artName + ".png";
+            }
+        }
+
+        public string ArtUrl
+        {
+            get
+            {
+                return String.Format(ART_URL_TEMPLATE, ArtName);
+            }
+        }
+
+        public string WikiTitle
+        {
+            get
+            {
+                var wikiTitle = BaseName;
+                if (goodName.Contains(" (Kiosk"))
+                    wikiTitle += " (Kiosk Demo)";
+                return wikiTitle;
+            }
+        }
+
+        public string WikiLink
+        {
+            get
+            {
+                StringBuilder builder = new StringBuilder();
+                var wikiTitle = Regex.Replace(WikiTitle, @"\s", "-");
+                for (int i = 0; i < wikiTitle.Length; i++)
+                {
+                    char cc = wikiTitle[i];
+                    if (UnescapePredicate(cc))
+                        builder.Append(cc);
+                    else
+                        builder.AppendFormat("%{0:X2}", (int)cc);
+                }
+                return builder.ToString();
+            }
+        }
+
+        private static bool UnescapePredicate(char cc)
+        {
+            // a - z
+            if (97 <= cc && cc <= 122)
+                return true;
+            // A - Z
+            else if (65 <= cc && cc <= 90)
+                return true;
+            // 0 - 9
+            else if (48 <= cc && cc <= 57)
+                return true;
+            // !
+            else if (cc == 33)
+                return true;
+            // '()*
+            else if (39 <= cc && cc <= 42)
+                return true;
+            // -.
+            else if (45 <= cc && cc <= 46)
+                return true;
+            // _
+            else if (cc == 95)
+                return true;
+            // ~
+            else if (cc == 126)
+                return true;
+            else
+                return false;
+        }
+
+        public string Description
+        {
+            get
+            {
+                StringBuilder builder = new StringBuilder();
+
+                if (refmd5 == null)
+                {
+                    builder.AppendLine(";" + goodName);
+                    builder.AppendLine(": MD5: " + md5);
+                    builder.AppendLine(": CRC: " + crc);
+                    builder.AppendLine(": Status: " + status);
+                    builder.AppendLine(": Players: " + players);
+                    builder.AppendLine(": Save Type: " + saveType);
+                    builder.AppendLine(": Rumble: " + rumble);
+                }
+                else
+                {
+                    builder.AppendLine(";" + goodName);
+                    builder.AppendLine(": MD5: " + md5);
+                    builder.AppendLine(": CRC: " + crc);
+                    builder.AppendLine(": See: " + refmd5);
+                }
+
+                return builder.ToString();
+            }
+        }
+    }
+}
